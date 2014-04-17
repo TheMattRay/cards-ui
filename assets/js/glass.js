8f5a19239f30137c58fd2930e16a006a867f0aca
Matt Ray <themattray@gmail.com>
2014-04-17 17:22:53 -0400

    Changes to provide working context

--- assets/js/glass.js
+++ assets/js/glass.js
@@ -3,6 +3,37 @@ var backCardMemory;
 
 $().ready(init);
 
+$(document).keydown(function(e){
+    switch(e.keyCode) { 
+    	case 37:
+       		//left
+       		prevCard();
+       		break;
+
+       	case 39:
+       		//right
+       		nextCard();
+       		break;
+
+       	case 38:
+       		//up
+       		break;
+
+       	case 40:
+       		//down
+       		break;
+    }
+});
+
+function wireClickEvent(card) {
+	console.log("wireClickEvent");
+	$(card).click(function() {
+		var code = $(this).attr("action");
+		console.log("evaluating: " + code);
+		eval(code);
+	});
+}
+
 function init() {
 	loadDefaultCard();
 }
@@ -56,13 +87,48 @@ function showBackCard() {
 
 function loadCard(card, setBreadcrumb) {
 	sweepStage();
-	$(card).clone().appendTo($(".content.stage"));
+	var newCard = $(card).clone();
+	wireClickEvent(newCard);
+	newCard.show();
+	newCard.appendTo($(".content.stage"));
 	if(setBreadcrumb == true) {
 		breadcrumbs[breadcrumbs.length] = $(card).attr("cardName");
 	}
 }
 
+function loadDynamicCard(text) {
+	//backCardMemory = $(".content.stage .card:visible").attr("cardName");
+	var thisCard = $(".content.system [cardName='blankCard']");
+	var thisCard = thisCard.clone();
+	thisCard.find("h1").text(text);
+	loadCard(thisCard);
+}
+
+function loadCardSetByName(cardSetName, setBreadcrumb) {
+	var cardSet = $("[cardSetName='" + cardSetName + "']");
+	loadCardSet(cardSet, setBreadcrumb);
+}
+
+function loadCardSet(cardSet, setBreadcrumb) {
+	var thisCard = $(".content.library [cardName='" + $(".content.stage .card:visible").attr("cardName") + "']")
+	sweepStage();
+	console.log("cardSet:");
+	console.log(cardSet);
+	var newCard = $($(cardSet).find("[cardName]")[0]).clone();
+	wireClickEvent(newCard);
+	newCard.show();
+	newCard.appendTo($(".content.stage"));
+	console.log(newCard);
+	//if(setBreadcrumb == true) {
+		breadcrumbs[breadcrumbs.length] = $(thisCard).attr("cardName");
+	//}
+}
+
 function loadDefaultCard() {
-	var firstCard = $(".content.library").children()[0];
-	$(firstCard).clone().appendTo($(".content.stage"));
+	var firstCardSet = $(".content.library .cardSet[cardSetName='home']");
+	var firstCard = firstCardSet.children().first();
+	firstCard = $(firstCard).clone();
+	wireClickEvent(firstCard);
+	firstCard.show();
+	firstCard.appendTo($(".content.stage"));
 }
